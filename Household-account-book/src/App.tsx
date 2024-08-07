4ead4c6421e4408b6cb615c8081208eee023f4ff
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-07 22:28:42 +0900

    fontの設定

--- Household-account-book/src/App.tsx
+++ Household-account-book/src/App.tsx
@@ -5,20 +5,28 @@ import Home from './components/Home'
 import Report from './components/report'
 import Nomatch from './components/Nomatch'
 import AppLayout from './components/AppLayout'
+import { Theme } from './theme/theme';
+import { ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
+
 
 function App() {
   return (
     //リンク先を表示するルートコンポーネントを準備
-    <Router>
-      <Routes>
-        <Route path='/' element={<AppLayout/>}>
-          <Route index element={<Home/>}></Route>
-          <Route path='report' element={<Report/>}></Route>
-          {/* //マッチしない場合は、これのリンクへと飛ばす */}
-          <Route path='*' element={<Nomatch/>}></Route>
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={Theme}>
+      {/* //ブラウザに正しく表示させるためのもの */}
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path='/' element={<AppLayout/>}>
+            <Route index element={<Home/>}></Route>
+            <Route path='report' element={<Report/>}></Route>
+            {/* //マッチしない場合は、これのリンクへと飛ばす */}
+            <Route path='*' element={<Nomatch/>}></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
 
---
+++ Household-account-book/src/theme/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+export const  Theme = createTheme({
+    typography: {
+        fontFamily: 'Noto Sans JP , Roboto , sans-serif',
+        fontWeightRegular : 400,
+        fontWeightMedium : 500,
+        fontWeightBold : 700
+    }
+})
+
