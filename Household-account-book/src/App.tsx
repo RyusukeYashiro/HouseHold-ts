3deba0572ed4aac1543a365958339f9a3cc669e9
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-07 14:22:05 +0900

    sidebarコンポーネントとMUIの実装

--- Household-account-book/src/App.tsx
+++ Household-account-book/src/App.tsx
@@ -4,15 +4,18 @@ import './App.css'
 import Home from './components/Home'
 import Report from './components/report'
 import Nomatch from './components/Nomatch'
+import AppLayout from './components/AppLayout'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='report' element={<Report/>}></Route>
-        {/* //マッチしない場合は、これのリンクへと飛ばす */}
-        <Route path='*' element={<Nomatch/>}></Route>
+        <Route path='/' element={<AppLayout/>}>
+          <Route index element={<Home/>}></Route>
+          <Route path='report' element={<Report/>}></Route>
+          {/* //マッチしない場合は、これのリンクへと飛ばす */}
+          <Route path='*' element={<Nomatch/>}></Route>
+        </Route>
       </Routes>
     </Router>
   )
