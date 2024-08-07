8862ed84c5fbe617e64375c8edc9789c0cfa0650
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-08 02:00:32 +0900

    homeコンポーネント

--- Household-account-book/src/components/Home.tsx
+++ Household-account-book/src/components/Home.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Box } from "@mui/material"
+import MonthlySummary from "./layout/MonthlySummary"
+import Calender from "./layout/Calender"
+import TransactionMenu from "./layout/TransactionMenu"
+import TransactionForm from "./layout/TransactionForm"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box sx={{display : "flex"}}>
+      {/* 左側に表示するコンテンツ */}
+      <Box sx={{flexGrow : 1}}>
+        <MonthlySummary/>
+        <Calender/>
+      </Box>
+      {/* 右側に表示するコンテンツ */}
+      <Box>
+        <TransactionMenu/>
+        <TransactionForm/>
+      </Box>
+    </Box>
   )
 }
 
