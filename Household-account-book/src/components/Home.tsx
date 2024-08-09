8bef3e05926f4484ea69170a9e5cd1a3567396f3
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-09 21:47:54 +0900

    引数の型をチェック

--- Household-account-book/src/components/Home.tsx
+++ Household-account-book/src/components/Home.tsx
@@ -3,13 +3,18 @@ import MonthlySummary from "./layout/MonthlySummary"
 import Calender from "./layout/Calender"
 import TransactionMenu from "./layout/TransactionMenu"
 import TransactionForm from "./layout/TransactionForm"
+import { Transaction } from "../types"
 
-const Home = () => {
+interface HomeProps {
+  monthlyTransactions : Transaction[],
+}
+
+const Home = ({ monthlyTransactions } : HomeProps) => {
   return (
     <Box sx={{display : "flex"}}>
       {/* 左側に表示するコンテンツ */}
       <Box sx={{flexGrow : 1}}>
-        <MonthlySummary/>
+        <MonthlySummary monthlyTransactions={monthlyTransactions}/>
         <Calender/>
       </Box>
       {/* 右側に表示するコンテンツ */}
