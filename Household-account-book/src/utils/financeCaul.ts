3295e9e5af7223dd0e723e2033859fed1110793c
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-09 21:56:12 +0900

    計算関数

---
+++ Household-account-book/src/utils/financeCaul.ts
@@ -0,0 +1,15 @@
+import { Balance, Transaction } from "../types";
+
+export function financeCaul(transactions : Transaction[]) : Balance {
+    //この関数のreturnとして
+    // {income: 300 , expense: 200 , balance: 200} のような形が理想
+    return transactions.reduce((acc , transaction) => {
+        if(transaction.type == "income") {
+            acc.income += transaction.amount
+        } else {
+            acc.expense += transaction.amount
+        }
+        acc.balance = acc.income - acc.expense;
+        return acc 
+    } , {income: 0 , expense: 0 , balance: 0});
+}
