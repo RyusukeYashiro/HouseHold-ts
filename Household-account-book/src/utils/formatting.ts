2368877310807dd719c044c01296820dd0948f46
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-16 15:35:14 +0900

    logを追加

--- Household-account-book/src/utils/formatting.ts
+++ Household-account-book/src/utils/formatting.ts
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 //日付をformatに適応させる関数
 export function formatMonth(date : Date) : string {
+    // console.log("成形する前の月データ" , date);
     return format(date , "yyyy-MM");
 }
