c9aec275a0a92c60bf0313469fb8462796a58c4d
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-08-25 20:49:23 +0900

    feat : グラフを表示

--- Household-account-book/src/components/BarChart.tsx
+++ Household-account-book/src/components/BarChart.tsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartData
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { Transaction } from '../types';
+import { dailyProcess } from '../utils/dailyProcess';
+import { Theme } from '../theme/theme';
+import { Box, CircularProgress, Typography } from '@mui/material';
 
-const BarChart = () => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface BarChartProps {
+  monthlyTransactions : Transaction[];
+  isLoading : boolean
+}
+
+const BarChart = ({monthlyTransactions , isLoading} : BarChartProps) => {
+  
+  
+  const options = {
+    maintainAspectRatio : false,
+    responsive: true,
+    plugins: {
+      // legend: {
+      //   position: "top"
+      // },
+      title: {
+        display: true,
+        text: "日別収支"
+      }
+    }
+  };
+
+  const dailyBalances = dailyProcess(monthlyTransactions);
+  console.log("reportの情報" , dailyBalances);
+
+  const dateLabel = dailyBalances.map(item => item.start);
+  const dateIncomes = dailyBalances.map(item => item.extendedProps.income);
+  const dateExpenses = dailyBalances.map(item => item.extendedProps.expense);
+
+  // console.log(dateLabel , dateIncomes , dateExpenses);
+  
+  const data : ChartData<"bar"> = {
+    labels : dateLabel,
+    datasets: [
+      {
+        label: "支出",
+        data : dateExpenses,
+        backgroundColor: Theme.palette.expenseColor.light
+      },
+      {
+        label: "収入",
+        data: dateIncomes,
+        backgroundColor: Theme.palette.incomeColor.light
+      }
+    ]
+  };
+
   return (
-    <div>BarChart</div>
+    <Box sx={{
+        flexGrow : 1,
+        display : 'flex',
+        alignItems : 'center',
+        justifyContent : 'center'
+      }}>
+      {isLoading ? (
+      <CircularProgress />
+    ) : monthlyTransactions .length > 0 ? 
+    ( <Bar options={options} data={data} /> ) :
+    <Typography>データがありません</Typography>
+    }
+    </Box>
   )
 }
 
